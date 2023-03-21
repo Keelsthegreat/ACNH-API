84e142d2c51681810f9396a160f9371d60ee7d1b
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-03-21 10:10:41 -0400

    .

--- api-project/src/Models/InsectDeets.jsx
+++ api-project/src/Models/InsectDeets.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Fish from "../Components/Fish";
 
 function InsectDetails({bug}) {
 return(
     <div className="display">
         <h2>{bug.name['name-USen']}</h2>
         <img className="image" src={bug.image_url} alt= {bug.name['name-USen']}/>
+
+        <dl>
+            <dt>Location:</dt>
+            <dd>{bug.ava}</dd>
+        </dl>
     </div>
 )
 }
