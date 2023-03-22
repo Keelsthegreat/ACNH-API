71fbf08fa92b989d935b70b3a4055ca99af5e768
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-03-22 17:28:54 -0400

    .

--- api-project/src/Models/VillagerDeets.jsx
+++ api-project/src/Models/VillagerDeets.jsx
@@ -5,6 +5,13 @@ function VillagerDetails({villager}) {
     <div className="display">
         <h2>{villager.name['name-USen']}</h2>
         <img className="image" src={villager.image_uri} alt= {villager.name['name-USen']}/>
+        <h4>{villager.personality}</h4>
+        <h4>{villager.birthday['birthday-string']}</h4>
+        <h4>{villager.species}</h4>
+        <h4>{villager.gender}</h4>
+       
+     
+            
     </div>
     )
 }
