2d170c8965886c1c559fcd2caa52f78d7f2382d9
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-03-26 16:13:34 -0400

    .

--- api-project/src/Components/AboutMe.jsx
+++ api-project/src/Components/AboutMe.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function AboutMe() {
   return (
-    <div>AboutMe</div>
+    <div>
+        <h1>Akili Inniss</h1>
+        <h2>Github</h2>
+        <a href='https://github.com/Keelsthegreat'></a>
+        <h2>LinkedIn Profile</h2>
+        <a href='https://www.linkedin.com/in/akili-inniss-778611265/'></a>
+    </div>
   )
 }
 
